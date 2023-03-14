1678a41c0aa4e37643a37df68b0a52adcd7a0462
asebeyakob@gmail.com <asebeyakob@gmail.com>
2023-03-13 23:33:31 -0600

    domlab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector('main#main')
+document.querySelector('main#main').remove();
+const newHeader = document.createElement("H1");
+const H1= document.getElementById("victory");
+  document.body.insertBefore(H1);
