97e46369d3110079a656698b749a24024abdd38b
asebeyakob@gmail.com <asebeyakob@gmail.com>
2023-03-19 12:23:08 -0600

    modifing element

--- index.js
+++ index.js
@@ -2,6 +2,5 @@
 document.querySelector('main#main')
 document.querySelector('main').remove();
 const newHeader = document.createElement("h1");
-h1.createElement="victory";
 newHeader.innerHTML =
 "<h1>YOUR-NAME is the champion</h1>";
